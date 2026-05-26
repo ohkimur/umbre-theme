0db730988b9312f1cd1d4c03a67d9348d773b2c5
Daniel (Ohkimur) Costrasel <ohkimur@gmail.com>
2026-05-26 03:00:33 +0300

    fix: refine syntax token color balance

--- src/theme/tokens/code.ts
+++ src/theme/tokens/code.ts
@@ -4,27 +4,20 @@ import { tokenRule } from "@/theme/token-rule.ts";
 import type { TokenColor } from "@/theme/types.ts";
 
 export const codeTokenColors = ({ syntax }: ThemeModel): TokenColor[] => [
-  tokenRule(
-    "Types",
-    [
-      "entity.name.type",
-      "entity.other.inherited-class",
-      "support.type",
-      "support.class",
-      "source.go storage.type",
-    ],
-    syntax.type,
-  ),
+  tokenRule("Types", ["entity.name.type"], syntax.type),
+  tokenRule("Inherited Class", ["entity.other.inherited-class"], syntax.tag),
+  tokenRule("Library Class or Type", ["support.type", "support.class", "source.go storage.type"], syntax.tag),
   tokenRule("Classes", ["entity.name.class", "entity.name.struct", "support.class.component"], syntax.class),
   tokenRule("Interfaces", ["entity.name.interface"], syntax.interface),
-  tokenRule("Function Name", ["entity.name.function", "support.function", "support.macro"], syntax.function),
+  tokenRule("Function Name", ["entity.name.function"], syntax.function),
+  tokenRule("Library Function", ["support.function", "support.macro"], syntax.markup),
   tokenRule(
     "Function Call",
     ["variable.function", "meta.function-call.generic", "support.function.go"],
     syntax.method,
   ),
-  tokenRule("Function Arguments", ["variable.parameter", "meta.parameter"], syntax.parameter),
-  tokenRule("Imports and Packages", ["entity.name.import", "entity.name.package"], syntax.string),
+  tokenRule("Function Arguments", ["variable.parameter", "meta.parameter"], syntax.foreground),
+  tokenRule("Imports and Packages", ["entity.name.import", "entity.name.package"], syntax.foreground),
   tokenRule("Entity Name", ["entity.name"], syntax.class),
   tokenRule("Tag", ["entity.name.tag", "meta.tag.sgml"], syntax.tag),
   tokenRule(
@@ -43,6 +36,17 @@ export const codeTokenColors = ({ syntax }: ThemeModel): TokenColor[] => [
       "entity.name.function.decorator",
     ],
     syntax.special,
+  ),
+  tokenRule(
+    "CSS Tag Names",
+    [
+      "source.css entity.name.tag",
+      "source.sass entity.name.tag",
+      "source.scss entity.name.tag",
+      "source.less entity.name.tag",
+      "source.stylus entity.name.tag",
+    ],
+    syntax.type,
   ),
   tokenRule("CSS Properties", ["support.type.property-name"], syntax.tag),
   tokenRule(
